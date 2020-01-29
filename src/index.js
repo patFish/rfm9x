12ed48d28c998c5ae5292d89b74cadd8ba4b5f12
patFish <fischer.pa7rick@gmail.com>
2020-01-29 15:12:00 +0100

    introduced first import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import http from 'http'
+import http from "http";
+import { Greeter } from "./greeter";
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World\n')
-}).listen(3000, '127.0.0.1')
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(Greeter("wixxa"));
+  })
+  .listen(3000, "127.0.0.1");
 
-console.log('Server running at http://127.0.0.1:3000/')
+console.log("Server running at http://127.0.0.1:3000/");
